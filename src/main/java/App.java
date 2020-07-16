4ee108d4dc58b766b2020439b4d1e4d32e918e0d
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-07-16 20:47:51 +0200

    Feature 3 on Feature branch added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("START");
 
+        //Add feature 1
+        //Add feature 2
+        //Add feature 3
+
         System.out.println("STOP");
     }
 }
