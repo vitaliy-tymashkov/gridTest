3fd541ccac820dcee11f0f5eec067c695635cad8
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-07-28 16:22:23 +0200

    Set stream filter added

--- src/main/java/RestAdapter.java
+++ src/main/java/RestAdapter.java
@@ -1,9 +1,19 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class RestAdapter {
 
     public static void withTest(){
         System.out.println("abstract static method");
+
+        Set<Integer> exec = new HashSet<>();
+
+//        exec.add(1);
+//        exec.add(2);
+//        exec.add(3);
+
+        Integer i = exec.stream().findFirst().orElse(22);
+        System.out.println("exec.stream() = " + i);
 
     }
 /*    public <T extends RestAdapter> T withTest(){
