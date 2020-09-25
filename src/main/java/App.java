db2cd2cde43b22cb574218662826fc4477e0d530
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-09-25 19:08:07 +0200

    SLF4J added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,19 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+//    Logger log = LoggerFactory.getLogger(getClass());
+//    static Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+
         System.out.println("START");
 
         //Add feature 1
         //Add feature 2
         //Add feature 3
-
+        log.info("*************************** INFO1 **************************************");
         RestAdapter.withTest();
 
         System.out.println("STOP");
